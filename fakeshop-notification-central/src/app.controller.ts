ff98a4414255b4ddf8acf32b5faa99dda81ae95f
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2023-03-17 23:35:01 -0300

    consumer kafka listening

--- fakeshop-notification-central/src/app.controller.ts
+++ fakeshop-notification-central/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import {
+  Ctx,
+  KafkaContext,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('fake-shop-login')
+  readMessage(@Payload() message: any, @Ctx() context: KafkaContext) {
+    const originalMessage = context.getMessage();
+    const response =
+      `Receiving a new message from topic: fake-shop-login: ` +
+      JSON.stringify(originalMessage.value);
+    console.log(response);
+    console.log(`Message: ${message}`)
+    return response;
   }
 }
--- fakeshop-notification-central/src/app.module.ts
+++ fakeshop-notification-central/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([
+    {
+      name: 'any_name_i_want',
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'fake-shop',
+        },
+      },
+    },
+  ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
